16d17b0cdbac15969dead271a002295f4002de57
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2022-04-17 13:46:14 +0700

    Update ins-vt.sh

--- ray/ins-vt.sh
+++ ray/ins-vt.sh
@@ -414,9 +414,9 @@ cat <<EOF > /etc/trojan/config.json
 {
     "run_type": "server",
     "local_addr": "0.0.0.0",
-    "local_port": 2087,
+    "local_port": 443,
     "remote_addr": "127.0.0.1",
-    "remote_port": 2603,
+    "remote_port": 89,
     "password": [
         "$uuid"
     ],
